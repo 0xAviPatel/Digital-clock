f560047e74725bdca4f649b3a6dda3e4ee1e5ad3
Avi <avip3513@gmail.com>
2022-09-25 21:43:55 +0530

    digital world

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CurrentTime from "./components/CurrentTime";
+
+const App = () => {
+  return (
+    <div>
+      <CurrentTime />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/CurrentTime.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const CurrentTime = () => {
+  let date = new Date();
+
+  let hour = date.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  });
+  const [time, setTime] = useState(hour);
+
+  const changeTime = () => {
+    
+    const newTime = new Date().toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: true,
+    })
+    setTime(newTime);
+  };
+
+  setInterval(() => {
+    changeTime()
+  }, 1000);
+
+  return (
+    <div>
+      <h2>{time}</h2>
+      <button onClick={changeTime}>Set Current Time</button>
+      {console.log(time)}
+    </div>
+  );
+};
+
+export default CurrentTime;
